6aa51edd09492c0a1421821bc6fdd4d080ddda53
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2013-06-21 08:07:42 +0000

    Import of NICS/Catmandu-Atom-0.02 from CPAN.
    gitpan-cpan-distribution: Catmandu-Atom
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         NICS/Catmandu-Atom-0.02.tar.gz
    gitpan-cpan-author:       NICS
    gitpan-cpan-maturity:     released

---
+++ lib/Catmandu/Atom.pm
@@ -0,0 +1,35 @@
+package Catmandu::Atom;
+
+=head1 NAME
+
+Catmandu::Atom - modules for working with Atom feeds
+
+=head1 VERSION
+
+Version 0.02
+
+=cut
+
+our $VERSION = '0.02';
+
+=head1 MODULES
+
+=over
+
+=item * L<Catmandu::Exporter::Atom>
+
+=item * L<Catmandu::Importer::Atom>
+
+=back
+
+=head1 AUTHOR
+
+Nicolas Steenlant, C<< <nicolas.steenlant at ugent.be> >>
+
+=head1 CONTRIBUTOR
+
+Vitali Peil, C<< <vitali.peil at uni-bielefeld.de> >>
+
+=cut
+
+1;
